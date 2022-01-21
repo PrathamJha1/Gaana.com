f1ec2c0cb2c1f94de178a41dd242243aa4f960f8
Pratham Jha <prathamjha5683@gmail.com>
2022-01-22 01:47:10 +0530

    .gitignore is now working

--- client/src/Components/Navbar/Player.js
+++
@@ -1,9 +0,0 @@
-import SpotifyPlayer from "react-spotify-web-playback";
-export default function Player(props) {
-    // console.log(accessToken);
-    if(!props.accessToken) return null;
-     console.log(props.track);
-    return(
-      <SpotifyPlayer token={props.accessToken} showSaveIcon uris={props.track?[props.track.uri]:[]}/>
-    );
-}
